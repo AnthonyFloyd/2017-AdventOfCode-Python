8a0f56006c6f8dbf233366c7849d9c136e5d80a3
Anthony Floyd <anthonyfloyd@gmail.com>
2017-12-03 11:29:02 -0800

    All tests pass, solution works, code is cleaned up.

--- day03.py
+++ day03.py
@@ -27,6 +27,8 @@ PART2_TESTS = [(2, 4),
 
 PART2_TARGET = PART1_END
 
+DEBUG = False
+
 # Solution
 
 def findCoordinates(squareID):
@@ -37,6 +39,8 @@ def findCoordinates(squareID):
     containing the integer coordinates of the square (x, y)
 
     '''
+
+    global DEBUG
 
     # The squares are arranged in a counter-clockwise spiral
     # The origin square, 1, is taken at (0, 0)
@@ -50,11 +54,13 @@ def findCoordinates(squareID):
     if squareID == 1:
         return (0, 0)
 
-    (size, totalSquares) = getLastFullRing(squareID)
+    size = getLastFullRingSize(squareID)
+
+    currentRingStartID = size ** 2 + 1
+    distanceFromRingStart = squareID - currentRingStartID
 
     # now we need to find the location of the target in the next ring
     size += 2
-    distanceFromRingStart = squareID - totalSquares - 1
 
     # ok, now find the (x, y) coordinates the distance along the ring
     # 3 = (1, 0)
@@ -66,64 +72,75 @@ def findCoordinates(squareID):
 
     # march around the ring
 
+    remainingDistance = distanceFromRingStart
+
     # up
-    if distanceFromRingStart <= (size - 2):
+    if remainingDistance <= (size - 2):
         # we only need to go up
-        location = [location[0], location[1] + distanceFromRingStart]
-        distanceFromRingStart = 0
+        location = [location[0], location[1] + remainingDistance]
+        remainingDistance = 0
     else:
         # go up as much as possible
         location = [location[0], location[1] + (size - 2)]
-        distanceFromRingStart -= (size - 2)
+        remainingDistance -= (size - 2)
 
     # left
-    if distanceFromRingStart <= (size - 1):
+    if remainingDistance <= (size - 1):
         # only need to go left
-        location = [location[0] - distanceFromRingStart, location[1]]
-        distanceFromRingStart = 0
+        location = [location[0] - remainingDistance, location[1]]
+        remainingDistance = 0
     else:
         # go left as much as possible
         location = [location[0] - (size - 1), location[1]]
-        distanceFromRingStart -= (size - 1)
+        remainingDistance -= (size - 1)
 
     # down
-    if distanceFromRingStart <= (size - 1):
+    if remainingDistance <= (size - 1):
         # only need to go down
-        location = [location[0], location[1] - distanceFromRingStart]
-        distanceFromRingStart = 0
+        location = [location[0], location[1] - remainingDistance]
+        remainingDistance = 0
     else:
         # go down as much as possible
         location = [location[0], location[1] - (size - 1)]
-        distanceFromRingStart -= (size - 1)
+        remainingDistance -= (size - 1)
 
     # can only go right now
-    location = [location[0] + distanceFromRingStart, location[1]]
+    location = [location[0] + remainingDistance, location[1]]
 
     # we're done! return the location
 
     return (location[0], location[1])
 
-def getLastFullRing(squareID):
+def getLastFullRingSize(squareID):
+    '''
+    Returns the size of the last full "ring" in the spiral.
+
+    Input is the squareID as an int, output is the ring size as an int.
+    '''
+
+    global DEBUG
 
     if squareID == 1:
-        return (1, 1)
-
-    if squareID <= 9:
-        return (1, 1)
+        return 1
 
     # Find the biggest ring that doesn't contain the target square
-    size = 1
-    totalSquares = 1
+
+    size = -1 # Need to start at zero, but sizes are odd, so
+    totalSquares = 1 # And, because we have a special exception for the first one, we need to count it
 
     while True:
         size += 2
         totalSquares += 4 * (size - 1)
 
+        # If squareID is bigger than then current number of squares
+        # and less than the current squares + the next layer
+        # then we've found the right ring
+
         if squareID > totalSquares  and squareID <= totalSquares + 4 * (size + 1):
             # found the right ring size
             break
 
-    return (size, totalSquares)
+    return size
 
 
 def findManhattanDistance(start, end):
@@ -134,9 +151,13 @@ def findManhattanDistance(start, end):
 
     '''
 
+    global DEBUG
+
+    # Find the start and end coordinates
     startLocation = findCoordinates(start)
     endLocation = findCoordinates(end)
 
+    # The Manhattan Distance is just the sum of the delta x and delta y
     distance = abs(startLocation[0] - endLocation[0]) + abs(startLocation[1] - endLocation[1])
 
     return int(distance)
@@ -149,47 +170,54 @@ def findNextAccumulation(target):
 
     '''
 
+    global DEBUG
+
+    # special exception for the first square
     if target == 1:
         return 1
 
     # We're going to need to keep track of old values in a matrix
     # The matrix size is no larger than the ring size that holds the target value
 
-    matrixSize = getLastFullRing(target)[0] + 4
-
+    matrixSize = getLastFullRingSize(target) + 4
     spiralMatrix = numpy.zeros((matrixSize, matrixSize), dtype=int)
 
     # populate the spiral until we exceed the target
     x1 = (matrixSize - 1) // 2
     y1 = x1
 
+    # start at the beginning
     location = [x1, y1] # (0, 0)
     total = 1
     spiralMatrix[location[0]][location[1]] = total
-
     currentSquareID = 1
 
+    # Loop until we've exceeded the target value
     while total <= target:
         # move to the next location
         currentSquareID += 1
 
         # do we need to move to the next ring?
 
-        (size, totalSquares) = getLastFullRing(currentSquareID)
-        currentRingStartID =  size ** 2 + 1
+        size = getLastFullRingSize(currentSquareID)
+        currentRingStartID = size ** 2 + 1
         distanceFromRingStart = currentSquareID - currentRingStartID
 
-        print('Square {:d} is {:d} from the start of the ring'.format(currentSquareID, distanceFromRingStart))
-        size += 2
+        if DEBUG:
+            print('Square {:d} is {:d} from the start of the ring'.format(currentSquareID, distanceFromRingStart))
+
+        size += 2 # Sizes are always odd
 
         if distanceFromRingStart == 0:
             # start a new ring, move right one, down one
-            print("Starting new ring")
-            print("Old location: {:d}, {:d}".format(location[0] - x1, location[1] - y1))
+            if DEBUG:
+                print("Starting new ring")
+                print("Old location: {:d}, {:d}".format(location[0] - x1, location[1] - y1))
+
             location = [location[0] + 1, location[1]]
-            #print("New location: {:d}, {:d}".format(location[0] - x1, location[1] - y1))
 
         else:
+            # move the current location based on where we are along the perimeter
             if distanceFromRingStart <= (size - 2):
                 # go up one
                 location = [location[0], location[1] + 1]
@@ -205,22 +233,29 @@ def findNextAccumulation(target):
 
         # find the value to put in the new spot
         # find sum of all neighbours
-        print("New location: {:d}, {:d}".format(location[0] - x1, location[1] - y1))
+        if DEBUG:
+            print("New location: {:d}, {:d}".format(location[0] - x1, location[1] - y1))
+
+        # For code clarity
         x = location[0]
         y = location[1]
 
-        total = int(spiralMatrix[x+1][y] + spiralMatrix[x+1][y+1] + spiralMatrix[x][y+1] + spiralMatrix[x-1][y+1] \
-            + spiralMatrix[x-1][y] + spiralMatrix[x-1][y-1] + spiralMatrix[x][y-1] + spiralMatrix[x+1][y-1])
 
-        # put this in this spot
+        # NB: Can't just assign this to spiralMatrix[x, y] because we break of the while loop based on total
+        # and x and y change as we march around the spiral
+
+        total = spiralMatrix[x+1][y] + spiralMatrix[x+1][y+1] + spiralMatrix[x][y+1] + spiralMatrix[x-1][y+1] \
+            + spiralMatrix[x-1][y] + spiralMatrix[x-1][y-1] + spiralMatrix[x][y-1] + spiralMatrix[x+1][y-1]
+
         spiralMatrix[x, y] = total
 
     # only get here if we found a value bigger than the target!
 
-    print("Location: {:d},{:d}".format(x, y))
-    print("{:6d} {:6d} {:6d}".format(spiralMatrix[x-1][y+1], spiralMatrix[x][y+1], spiralMatrix[x+1][y+1]))
-    print("{:6d} {:6d} {:6d}".format(spiralMatrix[x-1][y], spiralMatrix[x][y], spiralMatrix[x+1][y]))
-    print("{:6d} {:6d} {:6d}".format(spiralMatrix[x-1][y-1], spiralMatrix[x][y-1], spiralMatrix[x+1][y-1]))
+    if DEBUG:
+        print("Location: {:d},{:d}".format(x, y))
+        print("{:6d} {:6d} {:6d}".format(spiralMatrix[x-1][y+1], spiralMatrix[x][y+1], spiralMatrix[x+1][y+1]))
+        print("{:6d} {:6d} {:6d}".format(spiralMatrix[x-1][y], spiralMatrix[x][y], spiralMatrix[x+1][y]))
+        print("{:6d} {:6d} {:6d}".format(spiralMatrix[x-1][y-1], spiralMatrix[x][y-1], spiralMatrix[x+1][y-1]))
 
     return total
 
@@ -232,6 +267,8 @@ class TestDistance(unittest.TestCase):
 
     '''
 
+    global DEBUG
+
     # Part 1
 
     def test_part1(self):
@@ -239,6 +276,9 @@ class TestDistance(unittest.TestCase):
         Part 1 tests
 
         '''
+        global DEBUG
+
+        DEBUG = True
 
         for (end, distance) in PART1_TESTS:
             self.assertEqual(findManhattanDistance(1, end), distance)
@@ -250,6 +290,10 @@ class TestDistance(unittest.TestCase):
         Part 2 tests
 
         '''
+
+        global DEBUG
+
+        DEBUG = True
 
         for (target, nextResult) in PART2_TESTS:
             self.assertEqual(findNextAccumulation(target), nextResult)
